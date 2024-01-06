831bc02a40e7c719a8dfb7e7ce55455151833a92
fahadmemon1234 <fahadmemon131@gmail.com>
2024-01-06 17:12:37 +0500

    add file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Login from "./Component/Login/Login";
-import Navbar from "./Component/NavBar/Navbar";
+import Product from "./Component/Screen/Product/Product";
 import Home from "./Component/Home/Home";
 import { Route,Routes } from "react-router-dom";
 
@@ -10,6 +10,7 @@ function App () {
   <Routes>
     <Route path="/" element={<Login/>}></Route>
     <Route path="/Home" element={<Home/>}></Route>
+    <Route path="/Product" element={<Product/>}></Route>
   </Routes>
     </>
   )
---
+++ src/Component/Screen/Product/Product.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+// Main Page Connect
+// ---------------------------------------------------
+import Main from "../../NavBar/Navbar";
+// ---------------------------------------------------
+
+function Product() {
+  return (
+    <>
+      <Main>
+        <div className="card">
+          <div className="card-body">
+
+<div className="row" style={{paddingTop:'30px'}}>
+<div className="col-md-6 col-sm-6 col-lg-6">
+<h1>Product</h1>
+</div>
+
+<div className="col-md-6 col-sm-6 col-lg-6"></div>
+
+</div>
+            
+
+          </div>
+        </div>
+      </Main>
+    </>
+  );
+}
+
+export default Product;

0d4968b6e47c3689ef14fe52f329470b0c32750a
fahadmemon1234 <fahadmemon131@gmail.com>
2024-01-21 16:59:19 +0500

    add file

--- src/App.js
+++ src/App.js
@@ -5,19 +5,30 @@ import Home from "./Component/Home/Home";
 import Banks from "./Component/Screen/Bank/Bank";
 import Deposit from "./Component/Screen/Deposit/Deposit";
 import WithDraw from "./Component/Screen/WithDraw/WithDraw";
+import Customers from "./Component/Screen/Customers/Customers";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-        <Routes>
-          <Route path="/" element={<Login />}></Route>
-          <Route path="/Home" element={<Home />}></Route>
-          <Route path="/Product" element={<Product />}></Route>
-          <Route path="/Banks" element={<Banks />}></Route>
-          <Route path="/Deposit" element={<Deposit />}></Route>
-          <Route path="/WithDraw" element={<WithDraw />}></Route>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/Home" element={<Home />}></Route>
+
+        {/* ----------Product--------------------------- */}
+
+        <Route path="/Product" element={<Product />}></Route>
+
+        {/* ----------Banking--------------------------- */}
+
+        <Route path="/Banks" element={<Banks />}></Route>
+        <Route path="/Deposit" element={<Deposit />}></Route>
+        <Route path="/WithDraw" element={<WithDraw />}></Route>
+
+        {/* ----------Sales--------------------------- */}
+
+        <Route path="/Customers" element={<Customers />}></Route>
+      </Routes>
     </>
   );
 }
